e0a4668bb76886d920adb6fc975a86d75e3b1da6
Misheck Livingi <livingim2018@gmail.com>
2023-03-20 21:20:27 +0200

    Create Layout file

---
+++ src/Components/Layout/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import footer from './footer'
+import Navigation from './Navigation'
+
+const layout = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default layout
